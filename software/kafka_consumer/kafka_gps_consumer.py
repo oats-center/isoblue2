a9dd94b8d816aff1bb5c7f8a4eb3bc28039ff022
Yang Wang <wang701@purdue.edu>
2016-10-20 15:20:49 -0400

    Add: CAN consumer with arguments
    Delete old consumer scripts.

--- software/kafka_consumer/kafka_gps_consumer.py
+++ software/kafka_consumer/kafka_gps_consumer.py
@@ -5,15 +5,20 @@ import avro.io
 import io
 
 from kafka import KafkaConsumer
+from datetime import datetime
 
-consumer = KafkaConsumer("gps")
+consumer = KafkaConsumer("gps", auto_offset_reset="earliest", group_id=None)
+#consumer = KafkaConsumer("gps")
 schema_path = "../kafka_gps_log/gps.avsc"
 schema = avro.schema.parse(open(schema_path).read())
 
 for msg in consumer:
-    print "***************************"
     bytes_reader = io.BytesIO(msg.value)
     decoder = avro.io.BinaryDecoder(bytes_reader)
     reader = avro.io.DatumReader(schema)
     user1 = reader.read(decoder)
+
+    if user1["time"] is None or user1["lat"] is None or user1["lon"] is None:
+	continue
+
     print user1
