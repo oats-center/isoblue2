e115ae6a91f9f4b6ca626e87f1549ea725f0d80c
Yang Wang <wang701@purdue.edu>
2017-03-06 23:05:39 -0500

    Update: kafka_gps_log and gps schema
    Use nested schema for received GPS objects. For kafka_gps_log, it seems
    like the client is still polling messages twice per second. Tried using
    s.next(1) but nothing changed that much. Right now, after receiving GPS
    message, it will ditch anything that has duplicated timestamp and does
    not produce the message to Kafka.

--- software/producer/kafka_gps_log/kafka_gps_log.py
+++ software/producer/kafka_gps_log/kafka_gps_log.py
@@ -8,7 +8,7 @@ import avro
 import avro.schema
 import avro.io
 
-from gps3.agps3threaded import AGPS3mechanism
+from gps3 import gps3
 
 from kafka import KafkaProducer
 
@@ -20,8 +20,7 @@ schema_path = '/opt/schema/gps.avsc'
 isoblue_id_path = '/opt/id'
 #schema_path = '/home/yang/source/isoblue2/software/schema/gps.avsc'
 #isoblue_id_path = '/home/yang/source/isoblue2/test/uuid1'
-timestamp_last = None
-duplicates = 0
+timestamp_last = 0
 
 if __name__ == "__main__":
 
@@ -44,82 +43,81 @@ if __name__ == "__main__":
     except IOError:
         print('cannot open isoblue_id file')
         sys.exit()
-        
-    # set up gps thread
-    agps_thread = AGPS3mechanism()
-    agps_thread.stream_data()
-    agps_thread.run_thread()
 
-    while True:
-        timestamp = None
-        satellites = None
+    s = gps3.GPSDSocket()
+    s.connect(host='127.0.0.1', port=2947)
+    s.watch()
+    data_stream = gps3.DataStream()
 
-        # copy the object
-        data_stream_copy = copy.deepcopy(agps_thread.data_stream)
+    try:
+        for new_data in s:
+            if new_data:
+                # unpack the data stream
+                data_stream.unpack(new_data)
 
-        # set 'n/a' to None for object attributes
-        for attr, value in data_stream_copy.__dict__.iteritems():
-            #print attr, 'is', value
-            if value == 'n/a':
-                #print '*****************'
-                #print attr, 'is n/a'
-                setattr(data_stream_copy, attr, None)
-                #print 'now', attr, 'is', getattr(data_stream_copy, attr)
-                #print '*****************'
+                # set 'n/a' to None for the purpose of avro serialization
+                for key, value in data_stream.TPV.iteritems():
+                    if value == 'n/a':
+                        data_stream.TPV[key] = None
+                for key, value in data_stream.SKY.iteritems():
+                    if value == 'n/a':
+                        data_stream.SKY[key] = None
 
-        # convert time to unix timestamp
-        if data_stream_copy.time is not None:
-            utc_dt = datetime.strptime(data_stream_copy.time, '%Y-%m-%dT%H:%M:%S.%fZ')
-            timestamp = int((utc_dt - datetime(1970, 1, 1)).total_seconds())
-            # ditch duplicate timestamps
-            if timestamp_last is not None and (timestamp_last - timestamp) == 0:
-                #print '!!!!!!!!!!!!!!!!!'
-                #print '\tduplicate'
-                #print '!!!!!!!!!!!!!!!!!'
-                duplicates = duplicates + 1
-                if duplicates > 10:
-                    print 'Too many duplicates, are we having good GPS fix?'
-                sleep(1)
-                continue
+                # get the unix timestamp
+                timestamp = None
+                if data_stream.TPV['time'] is not None:
+                    utc_dt = datetime.strptime(data_stream.TPV['time'], '%Y-%m-%dT%H:%M:%S.%fZ')
+                    timestamp = int((utc_dt - datetime(1970, 1, 1)).total_seconds())
+                    # ditch duplicate samples
+                    # TODO: is there a better way to receive message?
+                    if timestamp_last is not None and (timestamp_last - timestamp) == 0:
+                        continue
 
-        # convert satelittes message to string
-        if data_stream_copy.satellites is not None:
-            satellites = str(data_stream_copy.satellites)
+                sat_list = None
+                # convert satelittes message to string
+                if data_stream.SKY['satellites'] is not None:
+                    sat_list = list(data_stream.SKY['satellites'])
 
-        #print('---------------------')
-        #print(                   data_stream_copy.time)
-        #print('UNIX ts:{}    '.format(timestamp))
-        #print('UNIX ts_last:{}    '.format(timestamp_last))
-        #print('Lat:{}   '.format(data_stream_copy.lat))
-        #print('Lon:{}   '.format(data_stream_copy.lon))
-        #print('Speed:{} '.format(data_stream_copy.speed))
-        #print('---------------------')
+                # create avro GPS datum
+                gps_datum = avro.io.DatumWriter(schema)
+                bytes_writer = io.BytesIO()
+                encoder = avro.io.BinaryEncoder(bytes_writer)
 
-        gps_datum = avro.io.DatumWriter(schema)
-        bytes_writer = io.BytesIO()
-        encoder = avro.io.BinaryEncoder(bytes_writer)
+                datum = {}
+                datum['TPV'] = {
+                        'timestamp': timestamp,
+                        'lat': data_stream.TPV['lat'],
+                        'lon': data_stream.TPV['lon'],
+                        'alt': data_stream.TPV['alt'],
+                        'epx': data_stream.TPV['epx'],
+                        'epy': data_stream.TPV['epy'],
+                        'epv': data_stream.TPV['epv'],
+                        'track': data_stream.TPV['track'],
+                        'speed': data_stream.TPV['speed'],
+                        'climb': data_stream.TPV['climb'],
+                        'epd': data_stream.TPV['epd'],
+                        'eps': data_stream.TPV['eps'],
+                        'epc': data_stream.TPV['epc']
+                        }
+                datum['SKY'] = {
+                        "xdop": data_stream.SKY['xdop'],
+                        "ydop": data_stream.SKY['ydop'],
+                        "vdop": data_stream.SKY['vdop'],
+                        "tdop": data_stream.SKY['tdop'],
+                        "hdop": data_stream.SKY['hdop'],
+                        "pdop": data_stream.SKY['pdop'],
+                        "gdop": data_stream.SKY['gdop'],
+                        "satellites": sat_list 
+                        }
 
-        gps_datum.write({
-            "timestamp":timestamp,
-            "lat":data_stream_copy.lat,
-            "lon":data_stream_copy.lon,
-            "alt":data_stream_copy.alt,
-            "epx":data_stream_copy.epx,
-            "epy":data_stream_copy.epy,
-            "epv":data_stream_copy.epv,
-            "track":data_stream_copy.track,
-            "speed":data_stream_copy.speed,
-            "climb":data_stream_copy.climb,
-            "epd":data_stream_copy.epd,
-            "eps":data_stream_copy.eps,
-            "epc":data_stream_copy.epc,
-            "satellites":satellites 
-            },
-            encoder)
+                # write to the datum
+                gps_datum.write(datum, encoder)
+                
+                # produce the message to Kafka
+                gps_buf = bytes_writer.getvalue()
+                producer.send(topic, key='gps:' + isoblue_id, value=gps_buf)
 
-        gps_buf = bytes_writer.getvalue()
-        producer.send(topic, key='gps:' + isoblue_id, value=gps_buf)
+                timestamp_last = timestamp
 
-        timestamp_last = timestamp
-
-        sleep(1)
+    except KeyboardInterrupt:
+        s.close()
