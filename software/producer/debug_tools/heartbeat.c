3d157b6c5dbe86046632653d32853c6f6934dec9
Yang Wang <wang701@purdue.edu>
2018-10-17 09:50:15 -0400

    Add: new heartbeat program

--- software/producer/debug_tools/heartbeat.c
+++ software/producer/debug_tools/heartbeat.c
@@ -39,167 +39,203 @@
 
 static char key[100];
 static char *brokers = "localhost:9092";
-static char *command = "qmicli -p -d /dev/cdc-wdm0 --nas-get-signal-strength | \
+static char *ns_cmd = "qmicli -p -d /dev/cdc-wdm0 --nas-get-signal-strength | \
   head -n 3 | tail -n 1 | sed \"s/^.*'\\([-0-9]*\\) dBm'[^']*$/\\1/\"";
+static char *led4_cmd = "cat /sys/class/leds/LED_4_GREEN/brightness";
+static char *led5_cmd = "cat /sys/class/leds/LED_5_GREEN/brightness";
 
 const char D_HB_SCHEMA[] =
 "{\"type\":\"record\",\
   \"name\":\"dhb\",\
   \"fields\":[\
-	{\"name\": \"timestamp\", \"type\": \"double\"},\
-	{\"name\": \"ns\", \"type\": \"int\"},\
-	{\"name\": \"netled\", \"type\": \"boolean\"},\
-	{\"name\": \"statled\", \"type\": \"boolean\"}]}";
+  {\"name\": \"timestamp\", \"type\": \"double\"},\
+  {\"name\": \"cellns\", \"type\": \"int\"},\
+  {\"name\": \"wifins\", \"type\": \"int\"},\
+  {\"name\": \"netled\", \"type\": \"boolean\"},\
+  {\"name\": \"statled\", \"type\": \"boolean\"}]}";
 
 /* Timer handler */
 void timer_handler(int signum) {
-	/* Kafka static variables */
-	static rd_kafka_t *rk = NULL;
-	static rd_kafka_topic_t *rkt = NULL;
-	static rd_kafka_conf_t *conf = NULL;
-	static rd_kafka_topic_conf_t *topic_conf = NULL;
-	static char errstr[512];
+  /* Kafka static variables */
+  static rd_kafka_t *rk = NULL;
+  static rd_kafka_topic_t *rkt = NULL;
+  static rd_kafka_conf_t *conf = NULL;
+  static rd_kafka_topic_conf_t *topic_conf = NULL;
+  static char errstr[512];
 
-	/* Avro static variables */
-	static avro_writer_t writer = NULL;
-	static avro_schema_t d_hb_schema = NULL;
-	static avro_datum_t d_hb = NULL;
-	static char buf[20];
+  /* Avro static variables */
+  static avro_writer_t writer = NULL;
+  static avro_schema_t d_hb_schema = NULL;
+  static avro_datum_t d_hb = NULL;
+  static char buf[20];
 
-	/* timeval struct */
-	struct timeval tp;
-	double timestamp;
+  /* timeval struct */
+  struct timeval tp;
+  double timestamp;
 
-	/* Heartbeat message variables */
-	static int ns;
+  /* Heartbeat message variables */
+  static int cell_ns;
+  static int wifi_ns = -70; //TODO: get real wifi rssi
   static int ret;
-  static bool netled = true;
-  static bool statled = true;
-	static FILE *fn;
+  static bool netled = false;
+  static bool statled = false;
+  static FILE *fn;
 
-	/* Broker conf */
-	if (conf == NULL) {
-		conf = rd_kafka_conf_new();
-		/* Kafka conf */
-		rd_kafka_conf_set(conf, "batch.num.messages", "20000", errstr,
-				sizeof(errstr));
-		rd_kafka_conf_set(conf, "queue.buffering.max.messages", "1000000", errstr,
-				sizeof(errstr));
-		rd_kafka_conf_set(conf, "queue.buffering.max.ms", "1", errstr,
-				sizeof(errstr));
-		rd_kafka_conf_set(conf, "log.connection.close", "false", errstr,
-				sizeof(errstr));
-	}
+  /* Broker conf */
+  if (conf == NULL) {
+    conf = rd_kafka_conf_new();
+    /* Kafka conf */
+    rd_kafka_conf_set(conf, "batch.num.messages", "20000", errstr,
+      sizeof(errstr));
+    rd_kafka_conf_set(conf, "queue.buffering.max.messages", "1000000", errstr,
+      sizeof(errstr));
+    rd_kafka_conf_set(conf, "queue.buffering.max.ms", "1", errstr,
+      sizeof(errstr));
+    rd_kafka_conf_set(conf, "log.connection.close", "false", errstr,
+      sizeof(errstr));
+  }
 
-	/* Kafka topic conf */
-	if (topic_conf == NULL) {
-		topic_conf = rd_kafka_topic_conf_new();
-		rd_kafka_topic_conf_set(topic_conf, "request.required.acks", "0", errstr,
-				sizeof(errstr));
-	}
+  /* Kafka topic conf */
+  if (topic_conf == NULL) {
+    topic_conf = rd_kafka_topic_conf_new();
+    rd_kafka_topic_conf_set(topic_conf, "request.required.acks", "0", errstr,
+      sizeof(errstr));
+  }
 
-	/* Create Kafka producer */
-	if (rk == NULL) {
-		if (!(rk = rd_kafka_new(RD_KAFKA_PRODUCER, conf, errstr, sizeof(errstr)))) {
-			fprintf(stderr, "%% Failed to create new producer: %s\n", errstr);
-		}
-	}
+  /* Create Kafka producer */
+  if (rk == NULL) {
+    if (!(rk = rd_kafka_new(RD_KAFKA_PRODUCER, conf, errstr, sizeof(errstr)))) {
+      fprintf(stderr, "%% Failed to create new producer: %s\n", errstr);
+    }
+  }
 
-	if (rd_kafka_brokers_add(rk, brokers) == 0) {
-		fprintf(stderr, "%% No valid brokers specified\n");
-		exit(EXIT_FAILURE);
-	}
+  if (rd_kafka_brokers_add(rk, brokers) == 0) {
+    fprintf(stderr, "%% No valid brokers specified\n");
+    exit(EXIT_FAILURE);
+  }
 
-	if (rkt == NULL) {
-		/* Create new Kafka topic */
-		rkt = rd_kafka_topic_new(rk, "debug", topic_conf);
-	}
+  if (rkt == NULL) {
+    /* Create new Kafka topic */
+    rkt = rd_kafka_topic_new(rk, "debug", topic_conf);
+  }
 
-	/* Initialize the schema structure from JSON */
-	if (d_hb_schema == NULL) {
-		if (avro_schema_from_json_literal(D_HB_SCHEMA, &d_hb_schema)) {
-			fprintf(stderr, "Unable to parse d_hb schema\n");
-			exit(EXIT_FAILURE);
-		}
-	}
+  /* Initialize the schema structure from JSON */
+  if (d_hb_schema == NULL) {
+    if (avro_schema_from_json_literal(D_HB_SCHEMA, &d_hb_schema)) {
+      fprintf(stderr, "Unable to parse d_hb schema\n");
+      exit(EXIT_FAILURE);
+    }
+  }
 
-	/* Create avro writer */
-	if (writer == NULL) {
-		writer = avro_writer_memory(buf, sizeof(buf));
-	}
+  /* Create avro writer */
+  if (writer == NULL) {
+    writer = avro_writer_memory(buf, sizeof(buf));
+  }
 
-	/* Create avro record based on the schema */
-	if (d_hb == NULL) {
-		d_hb = avro_record(d_hb_schema);
-	}
+  /* Create avro record based on the schema */
+  if (d_hb == NULL) {
+    d_hb = avro_record(d_hb_schema);
+  }
 
-	/* Get UNIX timestamp */
-	gettimeofday(&tp, NULL);
-	timestamp = tp.tv_sec + tp.tv_usec / 1000000.0;
+  /* Get UNIX timestamp */
+  gettimeofday(&tp, NULL);
+  timestamp = tp.tv_sec + tp.tv_usec / 1000000.0;
 
-	/* Check if heartbeat */
-	ret = system("wget -q --spider http://google.com");
-	if (ret == -1) {
-		perror("system");
-		exit(EXIT_FAILURE);
-	}
+  /* Check if heartbeat */
+  ret = system("wget -q --spider http://google.com");
+  if (ret == -1) {
+    perror("system");
+    exit(EXIT_FAILURE);
+  }
 
   /* If network checking returns error */
-	if (WEXITSTATUS(ret) == 0) {
-		/* Indicate online */
-		system("echo 0 > /sys/class/leds/LED_4_RED/brightness");
-		system("echo 255 > /sys/class/leds/LED_4_GREEN/brightness");
+  if (WEXITSTATUS(ret) == 0) {
+    /* Indicate online */
+    system("echo 0 > /sys/class/leds/LED_4_RED/brightness");
+    system("echo 255 > /sys/class/leds/LED_4_GREEN/brightness");
 #if DEBUG
-		printf("%f: alive\n", timestamp);
-		fflush(stdout);
+    printf("%f: alive\n", timestamp);
+    fflush(stdout);
 #endif
-	} else {
-		/* Indicate offline */
-		system("echo 0 > /sys/class/leds/LED_4_GREEN/brightness");
-		system("echo 255 > /sys/class/leds/LED_4_RED/brightness");
+  } else {
+    /* Indicate offline */
+    system("echo 0 > /sys/class/leds/LED_4_GREEN/brightness");
+    system("echo 255 > /sys/class/leds/LED_4_RED/brightness");
 #if DEBUG
-		printf("%f: dead\n", timestamp);
-		fflush(stdout);
+    printf("%f: dead\n", timestamp);
+    fflush(stdout);
 #endif
 #if DEBUG
     printf("Try force udev triggering ...\n");
     fflush(stdout);
 #endif
     system("udevadm trigger /sys/class/net/wwan0");
-	}
+  }
 
-	/* Get the network strength from command */
-	fn = popen(command, "r");
-	if (fn != NULL) {
-		fscanf(fn, "%d", &ns);
-	} else {
-		perror("popen");
-		exit(EXIT_FAILURE);
-	}
+  /* Get the network strength from command */
+  fn = popen(ns_cmd, "r");
+  if (fn != NULL) {
+    fscanf(fn, "%d", &cell_ns);
+  } else {
+    perror("popen");
+    exit(EXIT_FAILURE);
+  }
 
-	/* Close the subprocess */
-	if (pclose(fn) < 0) {
-		perror("pclose");
-		exit(EXIT_FAILURE);
-	}
+  /* Close the subprocess */
+  if (pclose(fn) < 0) {
+    perror("pclose");
+    exit(EXIT_FAILURE);
+  }
 
 #if DEBUG
-	printf("%f: network strength is %d\n", timestamp, ns);
-	fflush(stdout);
+  printf("%f: network strength is %d\n", timestamp, cell_ns);
+  fflush(stdout);
 #endif
 
-	if (ns < -100) {
-		printf("%f: Network strength %d dBm doesn't make sense! Something WRONG!\n",
-				timestamp, ns);
-	}
+  if (cell_ns < -100) {
+    printf("%f: Network strength %d dBm doesn't make sense! Something WRONG!\n",
+      timestamp, cell_ns);
+  }
+
+  /* Check if LED4 is lit green */
+  fn = popen(led4_cmd, "r");
+  if (fn != NULL) {
+    fscanf(fn, "%d", &netled);
+  } else {
+    perror("popen");
+    exit(EXIT_FAILURE);
+  }
+
+  /* Close the subprocess */
+  if (pclose(fn) < 0) {
+    perror("pclose");
+    exit(EXIT_FAILURE);
+  }
+
+  /* Check if LED5 is lit green */
+  fn = popen(led5_cmd, "r");
+  if (fn != NULL) {
+    fscanf(fn, "%d", &statled);
+  } else {
+    perror("popen");
+    exit(EXIT_FAILURE);
+  }
+
+  /* Close the subprocess */
+  if (pclose(fn) < 0) {
+    perror("pclose");
+    exit(EXIT_FAILURE);
+  }
 
   avro_datum_t ts_datum = avro_double(timestamp);
-  avro_datum_t ns_datum = avro_int32(ns);
+  avro_datum_t cell_ns_datum = avro_int32(cell_ns);
+  avro_datum_t wifi_ns_datum = avro_int32(wifi_ns);
   avro_datum_t netled_datum = avro_boolean(netled);
   avro_datum_t statled_datum = avro_boolean(statled);
 
   if (avro_record_set(d_hb, "timestamp", ts_datum)
-    || avro_record_set(d_hb, "ns", ns_datum)
+    || avro_record_set(d_hb, "cellns", cell_ns_datum)
+    || avro_record_set(d_hb, "wifins", wifi_ns_datum)
     || avro_record_set(d_hb, "netled", netled_datum)
     || avro_record_set(d_hb, "statled", statled_datum)) {
     fprintf(stderr, "Unable to set record to d_hb\n");
@@ -224,7 +260,8 @@ void timer_handler(int signum) {
 
   /* Decrement all our references to prevent memory from leaking */
   avro_datum_decref(ts_datum);
-  avro_datum_decref(ns_datum);
+  avro_datum_decref(cell_ns_datum);
+  avro_datum_decref(wifi_ns_datum);
   avro_datum_decref(netled_datum);
   avro_datum_decref(statled_datum);
 
@@ -233,65 +270,65 @@ void timer_handler(int signum) {
 }
 
 int main(int argc, char *argv[]) {
-	if (argv[1] == NULL) {
-		fprintf(stderr, "You need to supply ISOBlue ID file\n");
-		return EXIT_FAILURE;
-	}
+  if (argv[1] == NULL) {
+    fprintf(stderr, "You need to supply ISOBlue ID file\n");
+    return EXIT_FAILURE;
+  }
 
-	/* fp for opening uuid file */
-	FILE *fp;
-	char *id = 0;
-	long length;
+  /* fp for opening uuid file */
+  FILE *fp;
+  char *id = 0;
+  long length;
 
-	/* Timer stuff variables */
-	struct sigaction sa;
-	struct itimerval timer;
+  /* Timer stuff variables */
+  struct sigaction sa;
+  struct itimerval timer;
 
-	/* Get the id */
-	fp = fopen(argv[1], "r");
-	if (fp) {
-		fseek(fp, 0, SEEK_END);
-		length = ftell(fp);
-		fseek(fp, 0, SEEK_SET);
-		id = malloc(length);
-		if (id) {
-			fread(id, 1, length, fp);
-			if (id[length - 1] == '\n') {
-				id[--length] = '\0';
-			}
-		}
-		fclose(fp);
-	} else {
-		perror("ISOBlue ID file");
-		return EXIT_FAILURE;
-	}
+  /* Get the id */
+  fp = fopen(argv[1], "r");
+  if (fp) {
+    fseek(fp, 0, SEEK_END);
+    length = ftell(fp);
+    fseek(fp, 0, SEEK_SET);
+    id = malloc(length);
+    if (id) {
+      fread(id, 1, length, fp);
+      if (id[length - 1] == '\n') {
+        id[--length] = '\0';
+      }
+    }
+    fclose(fp);
+  } else {
+    perror("ISOBlue ID file");
+    return EXIT_FAILURE;
+  }
 
-	/* Create the key */
-	strcpy(key, "hb");
-	strcat(key, ":");
-	strcat(key, id);
+  /* Create the key */
+  strcpy(key, "hb");
+  strcat(key, ":");
+  strcat(key, id);
 
-	/* Install timer_handler as the signal handler for SIGALRM. */
-	memset (&sa, 0, sizeof(sa));
-	sa.sa_handler = &timer_handler;
-	sigaction(SIGALRM, &sa, NULL);
+  /* Install timer_handler as the signal handler for SIGALRM. */
+  memset (&sa, 0, sizeof(sa));
+  sa.sa_handler = &timer_handler;
+  sigaction(SIGALRM, &sa, NULL);
 
-	/* Configure the timer to expire after 10 secs... */
-	timer.it_value.tv_sec = 5;
-	timer.it_value.tv_usec = 0;
-	/* ... and every 10 secs after that. */
-	timer.it_interval.tv_sec = 5;
-	timer.it_interval.tv_usec = 0;
+  /* Configure the timer to expire after 5 secs... */
+  timer.it_value.tv_sec = 5;
+  timer.it_value.tv_usec = 0;
+  /* ... and every 10 secs after that. */
+  timer.it_interval.tv_sec = 5;
+  timer.it_interval.tv_usec = 0;
 
-	/* Start a real timer. It counts down whenever this process is
-	 * executing. */
-	setitimer(ITIMER_REAL, &timer, NULL);
+  /* Start a real timer. It counts down whenever this process is
+   * executing. */
+  setitimer(ITIMER_REAL, &timer, NULL);
 
-	system("echo 255 > /sys/class/leds/LED_4_RED/brightness");
+  system("echo 255 > /sys/class/leds/LED_4_RED/brightness");
 
-	while (1) {
-		sleep(1);
-	}
+  while (1) {
+    sleep(1);
+  }
 
-	return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
